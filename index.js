62b20c94e8936fc5e826e6d50aedc5e30780bf89
dung11205 <vandunght00000@gamil.com>
2024-11-05 14:50:54 +0700

    Your commit message

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Kết nối MongoDB
+mongoose.connect('mongodb+srv://dung:dung123@dung.dhy4z.mongodb.net/?retryWrites=true&w=majority&appName=dung', {
+
+
+}).then(() => {
+    console.log('Connected to MongoDB Atlas');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Thêm middleware để parse JSON requests
+
+// Định nghĩa route cho các API
+const tripRoute = require('./api/routes/trip.route');
+const addressRoute = require('./api/routes/address.route');
+const userRoute = require('./api/routes/users.route');
+
+app.use('/user', userRoute);
+app.use('/address', addressRoute);
+app.use('/trip', tripRoute);
+
+// Route chính
+app.get('/', (req, res) => res.send('Node.js'));
+
+// Khởi động server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
